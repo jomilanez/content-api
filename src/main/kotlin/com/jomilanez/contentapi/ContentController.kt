4cbb0da63a4d4894360ba47471ac00b59c50843a
jmilanez <josiane.milanez@gmail.com>
2019-04-12 07:27:41 +0200

    Simple version

--- src/main/kotlin/com/jomilanez/contentapi/ContentController.kt
+++ src/main/kotlin/com/jomilanez/contentapi/ContentController.kt
@@ -1,7 +1,5 @@
 package com.jomilanez.contentapi
 
-import io.swagger.annotations.Api
-import io.swagger.annotations.ApiOperation
 import org.springframework.http.HttpStatus.OK
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,20 +8,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
-@Api(
-    description = "Delivers content.",
-    tags = ["Default content"]
-)
-class DefaultContentController {
+class ContentController {
     @GetMapping("/content")
     @ResponseStatus(OK)
-    @ApiOperation("Delivers default content")
     fun getDefaultContent(
-    ): List<News> {
-        Thread.sleep(5000)
-        val listOf = listOf(News(id = "id", title = "sample title", category = "sports"))
-        return listOf
-    }
+    ): List<News> = listOf(News(id = "id", title = "sample title", category = "sports"))
 }
 
 data class News(
